a5886f985d8342f2f154987f51dce8292c1ff5d7
Timothy Adejumo <tadejumo12@gmail.com>
2022-04-06 14:58:46 +0100

    Initial Commit

--- Image.cpp
+++ Image.cpp
@@ -11,37 +11,9 @@
 
 bool Image::load(string filename)
 {
-    std::ifstream ifs;
-    ifs.open(filename, std::ios::binary);
-    // need to spec. binary mode for Windows users
-    try {
-        if (ifs.fail()) {
-            throw("Can't open input file");
-        }
-        std::string header;
-        int w, h, b;
-        ifs >> header;
-        if (strcmp(header.c_str(), "P6") != 0) throw("Can't read input file");
-        ifs >> w >> h >> b;
-        this->w = w;
-        this->h = h;
-        this->pixels = new Rgb[w * h]; // this is throw an exception if bad_alloc
-        ifs.ignore(256, '\n'); // skip empty lines in necessary until we get to the binary data
-        unsigned char pix[3]; // read each pixel one by one and convert bytes to floats
-        for (int i = 0; i < w * h; ++i) {
-            ifs.read(reinterpret_cast<char *>(pix), 3);
-            this->pixels[i].r = pix[0];
-            this->pixels[i].g = pix[1];
-            this->pixels[i].b = pix[2];
-        }
-        ifs.close();
-    }
-    catch (const char *err) {
-        fprintf(stderr, "%s\n", err);
-        ifs.close();
-    }
 
-    return true;
+    return false;
+
 }
 bool Image::loadRaw(string filename)
 {
