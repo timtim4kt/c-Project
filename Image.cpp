9eac43f807784fb8d84e05313f19a97d99b40a52
Timothy Adejumo <tadejumo12@gmail.com>
2022-04-07 19:04:16 +0100

    greyscale, rgb filter, and save ppm features added

--- Image.cpp
+++ Image.cpp
@@ -50,25 +50,63 @@ bool Image::loadRaw(string filename)
 }
 bool Image::savePPM(string filename)
 {
-    return false;
+    if (this->w == 0 || this->h == 0) { fprintf(stderr, "Can't save an empty image\n"); }
+    std::ofstream ofs;
+    try {
+        ofs.open(filename, std::ios::binary); // need to spec. binary mode for Windows users
+        if (ofs.fail()) throw("Can't open output file");
+        ofs << "P6\n" << this->w << " " << this->h << "\n255\n";
+        unsigned char r, g, b;
+        // loop over each pixel in the image, clamp and convert to byte format
+        for (int i = 0; i < this->w * this->h; ++i) {
+            r = static_cast<unsigned char>((this->pixels[i].r));
+            g = static_cast<unsigned char>((this->pixels[i].g));
+            b = static_cast<unsigned char>((this->pixels[i].b));
+            ofs << r << g << b;
+        }
+        ofs.close();
+    }
+    catch (const char *err) {
+        fprintf(stderr, "%s\n", err);
+        ofs.close();
+    }
+
+    return true;
+
 }
 
 
 void Image::filterRed()
 {
 
+    for (int i = 0; i < w * h; ++i) {
+        this->pixels[i].g = 0;
+        this->pixels[i].b = 0;
+    }
 }
 void Image::filterGreen()
 {
 
+    for (int i = 0; i < w * h; ++i) {
+        this->pixels[i].r = 0;
+        this->pixels[i].b = 0;
+    }
 }
 void Image::filterBlue()
 {
 
+    for (int i = 0; i < w * h; ++i) {
+        this->pixels[i].r = 0;
+        this->pixels[i].g = 0;
+    }
 }
 void Image::greyScale()
 {
-
+    for (int i = 0; i < w * h; ++i) {
+        this->pixels[i].r = pixels[i].r * 0.299 + pixels[i].g * 0.587 + pixels[i].b * 0.114;
+        this->pixels[i].g = pixels[i].r * 0.299 + pixels[i].g * 0.587 + pixels[i].b * 0.114;
+        this->pixels[i].b = pixels[i].r * 0.299 + pixels[i].g * 0.587 + pixels[i].b * 0.114;
+    }
 }
 void Image::flipHorizontal()
 {
